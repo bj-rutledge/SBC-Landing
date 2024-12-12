14b98be508f928c2b4689f6bd075929f8b276c6f
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2024-12-12 11:17:35 -0800

    change to use gatsby image. modify config file, add ts config for handling imports

--- Source/src/pages/projects.tsx
+++ Source/src/pages/projects.tsx
@@ -1,17 +1,12 @@
 // src/pages/projects.tsx
 import * as React from "react";
 import Layout from "../components/Layout";
-import { Box, Heading, Text } from "@chakra-ui/react";
-
+// import { Box, Heading, Text } from "@chakra-ui/react";
+import Map from "../components/Map";
 const ProjectsPage = () => (
   <Layout>
-    <Box p={5}>
-      <Heading as="h1">Projects</Heading>
-      <Text mt={4}>
-        Here you can find information about our past and current projects. Stay
-        tuned for updates on our latest work.
-      </Text>
-    </Box>
+    <Map></Map>
+   
   </Layout>
 );
 
