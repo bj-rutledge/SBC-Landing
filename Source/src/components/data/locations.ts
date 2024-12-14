932c95c491e9e29195ccdf7d986ca61676483fff
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2024-12-14 10:47:11 -0800

    modulize map component, move to import of locations, set up structre for reading in json file. todo, read in json file for locations.

--- Source/src/components/data/locations.ts
+++ Source/src/components/data/locations.ts
@@ -1,21 +1,21 @@
-/**
- * Created by BJ Rutledge
- * Date: 2024-12-13
- * Map marker locations
- **/
-import { Location } from "./types";
-import { Region } from "./types";
+import Img1 from '../../images/Illaria05.jpg'
+import Img2 from "../../images/Illaria06.jpg";
+import Img3 from "../../images/QATA_August_2013___1sm.jpg";
+import Img4 from "../../images/QATA_June_2013_Aerial_2sm.jpg";
+import {Location} from "../../types";
+
+type Region = "washington" | "hawaii";
+
 const locations: Record<Region, Location[]> = {
   washington: [
     {
-      address: "521 S. Weller Seattle WA 98104",
+      address: "123 Example St, Seattle, WA 98101",
       title: "Job Location 1",
       subtitle: "Project Alpha",
       phone: "123-456-7890",
       email: "info@joblocation1.com",
-      funFacts:
-        "This location was completed in 2020 and features sustainable building materials.",
-      imageSrc: '../../images/flyingPanels.jpg',
+      funFacts: "This location was completed in 2020 and features sustainable building materials.",
+      imageSrc: Img1,
       content: "Some Content!",
     },
     {
@@ -25,7 +25,7 @@ const locations: Record<Region, Location[]> = {
       phone: "987-654-3210",
       email: "info@joblocation2.com",
       funFacts: "This project won the best architecture award in 2019.",
-      imageSrc: "../../images/flyingPanels.jpg",
+      imageSrc: Img2,
       content: "",
     },
   ],
@@ -37,7 +37,7 @@ const locations: Record<Region, Location[]> = {
       phone: "808-123-4567",
       email: "info@hawaiijob1.com",
       funFacts: "This job features breathtaking ocean views.",
-      imageSrc: "../../images/flyingPanels.jpg",
+      imageSrc: Img3,
       content: "",
     },
     {
@@ -47,8 +47,7 @@ const locations: Record<Region, Location[]> = {
       phone: "808-987-6543",
       email: "info@hawaiijob2.com",
       funFacts: "This project includes eco-friendly construction methods.",
-      imageSrc:
-        "https://www.goconstruct.org/media/ec0pubko/hbf-construction-work-on-a-redrow-site-min.jpg?anchor=center&mode=crop&width=455&height=295&rnd=132659746009270000",
+      imageSrc: Img4,
       content: "",
     },
   ],
