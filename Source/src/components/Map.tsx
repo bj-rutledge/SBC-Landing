6dcce1f2533f3b253f00ffa4fd6a2584a4d56c37
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2024-12-14 10:47:11 -0800

    modulize map component, move to import of locations, set up structre for reading in json file. todo, read in json file for locations.

--- Source/src/components/Map.tsx
+++ Source/src/components/Map.tsx
@@ -8,14 +8,9 @@ import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client"; // Import createRoot
 import { Box, Select, Heading, Flex } from "@chakra-ui/react";
 import MapInfoCard from "./MapInfoCard";
-import { Location } from "./helpers/types";
+import { Location } from "../types";
 import useWindowSize from "../hooks/useWindowSize";
-
-// Import local images
-import Img1 from "../images/Illaria05.jpg";
-import Img2 from "../images/Illaria06.jpg";
-import Img3 from "../images/QATA_August_2013___1sm.jpg";
-import Img4 from "../images/QATA_June_2013_Aerial_2sm.jpg";
+import locations from "./data/locations"; // Import locations
 
 declare global {
   interface Window {
@@ -24,53 +19,6 @@ declare global {
 }
 
 type Region = "washington" | "hawaii";
-
-const locations: Record<Region, Location[]> = {
-  washington: [
-    {
-      address: "123 Example St, Seattle, WA 98101",
-      title: "Job Location 1",
-      subtitle: "Project Alpha",
-      phone: "123-456-7890",
-      email: "info@joblocation1.com",
-      funFacts: "This location was completed in 2020 and features sustainable building materials.",
-      imageSrc: Img1,
-      content: "Some Content!",
-    },
-    {
-      address: "456 Another St, Seattle, WA 98102",
-      title: "Job Location 2",
-      subtitle: "Project Beta",
-      phone: "987-654-3210",
-      email: "info@joblocation2.com",
-      funFacts: "This project won the best architecture award in 2019.",
-      imageSrc: Img2,
-      content: "",
-    },
-  ],
-  hawaii: [
-    {
-      address: "Example St, Honolulu, HI 96813",
-      title: "Hawaii Job 1",
-      subtitle: "Project Aloha",
-      phone: "808-123-4567",
-      email: "info@hawaiijob1.com",
-      funFacts: "This job features breathtaking ocean views.",
-      imageSrc: Img3,
-      content: "",
-    },
-    {
-      address: "Another St, Maui, HI 96793",
-      title: "Hawaii Job 2",
-      subtitle: "Project Paradise",
-      phone: "808-987-6543",
-      email: "info@hawaiijob2.com",
-      funFacts: "This project includes eco-friendly construction methods.",
-      imageSrc: Img4,
-      content: "",
-    },
-  ],
-};
 
 const Map: React.FC = () => {
   const [map, setMap] = useState<google.maps.Map | null>(null);
@@ -195,8 +143,6 @@ const Map: React.FC = () => {
       />
     </Flex>
   );
-  
-  
 };
 
 export default Map;
