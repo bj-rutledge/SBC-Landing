80e776087b29e666d1097c5de464fba664153322
BJ Rutledge <86386474+bj-rutledge@users.noreply.github.com>
2024-12-24 11:28:00 -0800

    edit getSheetData so that data written to the json file will be an object if only one object is in the data array. If more than one object, write the object array.

--- convert-sbc-jobs-to-json/getSheetData/index.js
+++ convert-sbc-jobs-to-json/getSheetData/index.js
@@ -5,7 +5,8 @@ const sheetDataToJson = (filePath, sheetName) => {
    const data = readInExcelSheet(filePath, 'Total Square Footage');
    //replace empty spaces in name with '-' before adding json extension
    const outputFileName = `${sheetName.replace(/ /g, '-')}.json`;
-   fs.writeFileSync(outputFileName, JSON.stringify(data, null, 2), 'utf-8');
+   const output = data.length > 1? data: data[0];
+   fs.writeFileSync(outputFileName, JSON.stringify(output, null, 2), 'utf-8');
 };
 
 const filePath = '../spreadsheets/sbc-website-jobs-list.xlsm';
